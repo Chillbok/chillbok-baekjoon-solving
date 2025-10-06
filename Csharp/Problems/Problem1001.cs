4a5bdb9b5ccfd7942154bc78342e2aeb5c6efda2
chillbok <kylejunho@gmail.com>
2025-10-06 17:12:03 +0900

    feat: 문제 1001 푸는 중
    일단 문제는 만들었는데, 이게 맞는지는 모르겠음.

---
+++ Csharp/Problems/Problem1001.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Problem1001
+{
+    public void Main()
+    {
+        string input = Console.ReadLine();
+        int A = int.Parse(input);
+
+        input = Console.ReadLine();
+        int B = int.Parse(input);
+
+        Console.WriteLine(A - B);
+    }
+}
--- Csharp/Problems/Program.cs
+++ Csharp/Problems/Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Problem1001 problem1001 = new Problem1001();
+problem1001.Main();
