811d155ccb27fc7a714a7cdb7dadfc8e18cf4441
chillbok <kylejunho@gmail.com>
2025-10-14 19:05:45 +0900

    feature: 1002번 문제 푸는 중
    조규현과 백승환 위치 구하는 것까지는 해결함.
    그런데 어떻게 0이 나오는 조건을 정할지를 모르겠음.

--- Csharp/Problems/1002/Solution.cs
+++ Csharp/Problems/1002/Solution.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.InteropServices;
 
 namespace Csharp.Problems
 {
@@ -6,6 +9,77 @@ namespace Csharp.Problems
     {
         public void Solve()
         {
+            int testCaseAmount = InputSingleNumber();
+
+            List<int> answer = new List<int>();
+
+            for (int i = 0; i < testCaseAmount; i++)
+            {
+                List<int> a = new List<int>();
+                a = InputMultipleNumber();
+
+                int distanceR1R2 = a[2] + a[5];
+                double distanceAB = GetLength(a[0], a[1], a[3], a[4]);
+
+                int testCaseResult = DefineTestCase(distanceR1R2, distanceAB);
+                answer.Add(testCaseResult);
+                Console.WriteLine($"두 좌표 거리: {distanceAB}, r_1 + r2 = {distanceR1R2}");
+            }
+
+            for (int i = 0; i < answer.Count; i++)
+            {
+                Console.WriteLine(answer[i]);
+            }
+        }
+
+        private int DefineTestCase(int r1R2Length, double lengthAB)
+        {
+            int ABNormalize = ReturnNormalNumber(lengthAB);
+            int r1r2Normalize = ReturnNormalNumber(r1R2Length);
+
+            if (ABNormalize < r1r2Normalize) { return 2; }
+            else if (ABNormalize == r1r2Normalize) { return 1; }
+            else { return -1; }
+        }
+
+        //숫자에 1000 곱한 뒤, int로 변환해서 반환
+        private int ReturnNormalNumber(double number)
+        {
+            double value = number * 1000;
+            return (int)value;
+        }
+
+        private int ReturnNormalNumber(int number)
+        {
+            number = number * 1000;
+            return number;
+        }
+
+        private int InputSingleNumber()
+        {
+            string[] inputs = Console.ReadLine()!.Split();
+            return int.Parse(inputs[0]);
+        }
+
+        private List<int> InputMultipleNumber()
+        {
+            string[] inputs = Console.ReadLine()!.Split();
+            List<int> answerList = new List<int>();
+
+            for (int i = 0; i < inputs.Length; i++)
+            {
+                answerList.Add(int.Parse(inputs[i]));
+            }
+            return answerList;
+        }
+
+        //길이 구하는 함수
+        private double GetLength(int a, int b, int c, int d)
+        {
+            double xValue = Math.Pow(b - a, 2);
+            double yValue = Math.Pow(c - d, 2);
+            double result = Math.Pow(xValue + yValue, 1.0 / 2.0);
+            return result;
         }
     }
 }
--- Csharp/obj/Debug/net8.0/Csharp.AssemblyInfo.cs
+++ Csharp/obj/Debug/net8.0/Csharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Csharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54e47bcecd5ef3a519651b064f7bc3323247562b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+311aaf2dbb1a72d8c139f0000d5a4efa1006cdf2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Csharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Csharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
