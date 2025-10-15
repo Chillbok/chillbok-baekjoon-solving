9e9ee0f6885821879b9619f3ff0da90e5c464f92
chillbok <kylejunho@gmail.com>
2025-10-15 22:45:54 +0900

    feat: 피보나치 수열 C#으로 구현

--- Csharp/Problems/1003/Solution.cs
+++ Csharp/Problems/1003/Solution.cs
@@ -1,9 +1,27 @@
+using System;
+
 namespace Csharp.Problems
 {
     public class Problem1003
     {
         public void Solve()
         {
+        }
+
+        private int fibonacci(int n)
+        {
+            if (n == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else if (n == 1)
+            {
+                Console.WriteLine(1);
+            }
+            else
+            {
+                return fibonacci(n - 1) + fibonacci(n - 2);
+            }
         }
     }
 }
