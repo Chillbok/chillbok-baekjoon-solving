29d5e79835f188f72c62fa5e0ac67b67f5acc402
chillbok <kylejunho@gmail.com>
2025-10-30 20:52:54 +0900

    refactor: 기존 3개였던 array를 2개로 줄이기 위한 작업 시작
    원래는 string array를 먼저 만들고,
    그 string array를 6개의 int 변수에 저장하는 방식으로 진행함
    이 방식이 지나치게 많은 변수를 생성하는 것 같음
    하나의 int array만 사용하도록 수정하려고 함
    
    이번 커밋에서는 기존 string array를 int array로 수정함

--- Csharp/Problems/1002/Solution.cs
+++ Csharp/Problems/1002/Solution.cs
@@ -9,53 +9,55 @@ namespace Csharp.Problems
     {
         public void Solve()
         {
-            string[] firstInput = Console.ReadLine()!.Split();
-            //테스트 횟수
-            int testCaseAmount = int.Parse(firstInput[0]);
+            //테스트 횟수 정하기
+            int testCaseAmount = GetInput()[0]; //테스트 횟수
 
-            List<int> answer = new List<int>();
+            int[] testCases = GetInput(); //테스트 값들 (x1, y1, r1, x2, y2, r2)
 
-            string[] numberInputs;
+            List<int> answer = new List<int>(); //답 저장하기 위한 리스트
 
+            //testCaseAmount만큼 반복 실행
             for (int i = 0; i < testCaseAmount; i++)
             {
-                numberInputs = Console.ReadLine()!.Split();
-
-                int x1, y1, r1, x2, y2, r2;
-
-                x1 = int.Parse(numberInputs[0]);
-                y1 = int.Parse(numberInputs[1]);
-                r1 = int.Parse(numberInputs[2]);
-                x2 = int.Parse(numberInputs[3]);
-                y2 = int.Parse(numberInputs[4]);
-                r2 = int.Parse(numberInputs[5]);
-
-                answer.Add(Test(x1, y1, r1, x2, y2, r2));
+                answer.Add(Test(testCases));
+                if (answer[i] == -100) { Console.WriteLine("배열 길이가 6이 아니므로 강제 종료함"); return; }
             }
 
-
-            for (int i = 0; i < answer.Count; i++)
-            {
-                Console.WriteLine(answer[i]);
-            }
+            //완성된 테스트케이스 하나씩 출력
+            for (int i = 0; i < answer.Count; i++) { Console.WriteLine(answer[i]); }
+        }
+        
+        //입력 받아서 int 배열로 나눠주는 함수
+        private static int[] GetInput()
+        {
+            string [] firstInput = Console.ReadLine()!.Split();
+            int firstInputLength = firstInput.Length;
+            int[] result = new int[firstInputLength];
+            for (int i = 0; i < firstInputLength; i++) { result[i] = int.Parse(firstInput[i]); }
+            return result;
         }
 
         //테스트 실행
-        private static int Test(int x1, int y1, int r1, int x2, int y2, int r2)
+        private static int Test(int[] arr)
+        //private static int Test(int x1, int y1, int r1, int x2, int y2, int r2)
         {
-            //두 원의 중심 사이 거리
-            long distAB = ReturnDistance(x1, y1, x2, y2);
+            //긴급 종료
+            if (arr.Length != 6) {return -100;}
 
-            if ((distAB == 0) && (r1 == r2)) { return -1; }
+            //두 원의 중심 사이 거리 구하기
+            long distAB = ReturnDistance(arr[0], arr[1], arr[3], arr[4]);
 
-            long sumR1R2 = (long)Math.Pow(r1 + r2, 2);
-            long minusR1R2 = (long)Math.Pow(r1 - r2, 2);
+            if ((distAB == 0) && (arr[2] == arr[5])) { return -1; }
+
+            long sumR1R2 = (long)Math.Pow(arr[2] + arr[5], 2);
+            long minusR1R2 = (long)Math.Pow(arr[2] - arr[5], 2);
 
             if (distAB > sumR1R2 || distAB < minusR1R2) { return 0; }
             else if (distAB == sumR1R2 || distAB == minusR1R2 ) { return 1; }
             else { return 2; }
         }
 
+        //두 점의 2차원 좌표 거리 구하는 함수
         private static long ReturnDistance(int x1, int y1, int x2, int y2)
         {
             double result = Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2);
